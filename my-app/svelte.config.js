9eec0a4f69b774692507bbce4acb6280fd47eb2c
nhrnhr0 <nhrnhr0@gmail.com>
2023-02-07 11:16:55 +0200

    adding server env

--- my-app/svelte.config.js
+++ my-app/svelte.config.js
@@ -1,9 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
+import node_adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		// adapter: adapter()
+		adapter: node_adapter({ out: 'build', envPrefix: 'SVELTEKIT_'
+		}),
+
 	}
 };
 
